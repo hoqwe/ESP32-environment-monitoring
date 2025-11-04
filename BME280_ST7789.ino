db49f4766019fd85972a46eaab4586f07770ed03
hoqwe <jobimegoni@gmail.com>
2025-11-04 12:46:35 +0500

    Fix text overlapping (partially)
    There is still an issue with black areas above the arc after cleaning
    long values, such as `-99.99` (6 chars) to `0.00` (4 chars).

--- BME280_ST7789.ino
+++ BME280_ST7789.ino
@@ -4,9 +4,9 @@
 Adafruit_BME280 bme;
 TFT_eSPI tft = TFT_eSPI();
 
-const int fontSize = 3, fontHeight = 8, fontWidth = 6;
-const int r = 55, arcWidth = 10;
-const int ir = r - arcWidth;
+// Font
+const int fontSize = 3;
+const int charHeight = 8 * fontSize, charWidth = 6 * fontSize;
 
 // Colors
 const int bg = 0x0000, alpha = 128;
@@ -15,8 +15,12 @@ const int hArcFg = 0x867D, hArcBg = tft.alphaBlend(alpha, hArcFg, bg);
 const int pArcFg = 0xD69A, pArcBg = tft.alphaBlend(alpha, pArcFg, bg);
 
 // Arc
+const int r = 55, arcWidth = 10;
+const int ir = r - arcWidth;
 const int startAngle = 45, endAngle = 315;
 const int arcDegrees = endAngle - startAngle;
+
+float tPrev = 0.0f, hPrev = 0.0f, pPrev = 0.0f;
 
 void setup() {
     bme.begin(0x76);
@@ -33,14 +37,15 @@ void loop() {
     float h = bme.readHumidity();
     float p = bme.readPressure() / 100.0f;
 
-    drawGauge(t, 0, 50, "\xF7" "C", 60, 60, tArcFg, tArcBg);
-    drawGauge(h, 0, 100, "%", 180, 60, hArcFg, hArcBg);
-    drawGauge(p, 960, 1020, "hPa", 120, 180, pArcFg, pArcBg);
+    drawGauge(t, tPrev, 0, 50, "\xF7" "C", 60, 60, tArcFg, tArcBg);
+    drawGauge(h, hPrev, 0, 100, "%", 180, 60, hArcFg, hArcBg);
+    drawGauge(p, pPrev, 960, 1020, "hPa", 120, 180, pArcFg, pArcBg);
 
+    tPrev = t, hPrev = h, pPrev = p;
     delay(1000);
 }
 
-void drawGauge(float value, int minValue, int maxValue, String unit,
+void drawGauge(float value, float valuePrev, int minValue, int maxValue, String unit,
                int x, int y, int arcFg, int arcBg) {
     // Background arc
     tft.drawSmoothArc(x, y, r, ir, startAngle, endAngle, arcBg, bg, true);
@@ -55,8 +60,12 @@ void drawGauge(float value, int minValue, int maxValue, String unit,
     }
 
     // Value
+    tft.setTextPadding(String(valuePrev, 2).length() * charWidth);
     tft.drawString(String(value, 2), x, y);
+    tft.setTextPadding(0);
+
+    // Unit
     tft.setTextSize(fontSize - 1);
-    tft.drawString(unit, x, y + fontSize*fontHeight);
+    tft.drawString(unit, x, y + charHeight);
     tft.setTextSize(fontSize);
 }
